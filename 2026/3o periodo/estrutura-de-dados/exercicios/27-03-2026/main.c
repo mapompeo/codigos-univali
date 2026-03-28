eb9290bffb1e31bdf7d9589a4c80017bef42440b
Matheus Pompeo <matheuspompeo13@gmail.com>
2026-03-27 21:49:25 -0300

    feat: implement open addressing insertion and printing for hash table

--- 2026/3o periodo/estrutura-de-dados/exercicios/27-03-2026/main.c
+++ 2026/3o periodo/estrutura-de-dados/exercicios/27-03-2026/main.c
@@ -26,6 +26,48 @@ int funcaoHash(int chave)
     return chave % tamanho;
 }
 
+int funcaoReHash(int chave)
+{
+    return chave++ % tamanho;
+}
+
+void inserirAberto(TabelaHash *tabela, Aluno alunoNovo)
+{
+    int id = funcaoHash(alunoNovo.matricula);
+
+    int i = 0;
+    while (i < tamanho)
+    {
+        int posicao = (id + i) % tamanho;
+        if (tabela->dados[posicao] == NULL)
+        {
+            No *novoNo = (No *)calloc(1, sizeof(No));
+            novoNo->aluno = alunoNovo;
+
+            tabela->dados[posicao] = novoNo;
+            return;
+        }
+        i++;
+    }
+    printf("Tabela cheia, não foi possível inserir o aluno %s\n,", alunoNovo.nome);
+}
+
+void imprimirAberto(TabelaHash *tabela)
+{
+    printf("Tabela Hash (Endereçamento Aberto):\n");
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (tabela->dados[i] != NULL)
+        {
+            printf("Posição [%d]: {Matricula: %d, Nome: %s}\n", i, tabela->dados[i]->aluno.matricula, tabela->dados[i]->aluno.nome);
+        }
+        else
+        {
+            printf("Posição [%d]: NULL\n", i);
+        }
+    }
+}
+
 void inserirExterior(TabelaHash *tabela, Aluno alunoNovo)
 {
     int id = funcaoHash(alunoNovo.matricula);
@@ -89,7 +131,28 @@ int main()
     Aluno *buscaNulo = buscaExterior(tabela, 5678);
 
     printf("[Mat: %d | Nome: %s]\n", buscaDaAlice->matricula, buscaDaAlice->nome);
-    printf("[Mat: ", buscaDaAlice->matricula, " | Nome: ", buscaDaAlice->nome, "]");
+    if (buscaNulo != NULL)
+    {
+        printf("[Mat: %d | Nome: %s]\n", buscaNulo->matricula, buscaNulo->nome);
+    }
+    else
+    {
+        printf("Aluno nao encontrado!\n");
+    }
+
+    printf("-----------------------------------------\n");
+
+    TabelaHash *tabelaAberto = (TabelaHash *)calloc(1, sizeof(TabelaHash));
+
+    Aluno felipe = {46, "felipe"};
+    Aluno lucas = {38, "lucas"};
+    Aluno maria = {46, "maria"};
+
+    inserirAberto(tabelaAberto, felipe);
+    inserirAberto(tabelaAberto, lucas);
+    inserirAberto(tabelaAberto, maria);
+
+    imprimirAberto(tabelaAberto);
 
     return 0;
 }
